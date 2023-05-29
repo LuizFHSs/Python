14ee1b080899f41b483c167a2739c7fb662e3887
LuizFHSs <devlfhss@gmail.com>
2023-05-29 00:09:25 -0300

    edit-condicionais.py-add-file-while.py

--- condicionais.py
+++ condicionais.py
@@ -17,7 +17,7 @@ media = float(input("Informe a média do estudante: "))
 
 if media >= 7:
     print("Você foi Aprovada(o)!")
-elif media >= 5:
+elif media >= 5:# Caso onde houvesse uma condição de recuperação, onde a média maior ou igual a 5 é recupração
     print("Recuperação")
 else:
     print("Você foi Reprovada(o)!")
