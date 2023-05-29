486fe2903fe2af02d90b91ff13ccdffdb1dbe730
LuizFHSs <devlfhss@gmail.com>
2023-05-28 22:49:47 -0300

    edit-file-operadores

--- operadores.py
+++ operadores.py
@@ -5,7 +5,33 @@
     - Subtração: -
     - Multiplicação: *
     - Divisão: /
-    - Divisão inteira: //
+    - Divisão inteira: // -> Retorna somente o valor inteiro da divisão
     - Resto da divisão: %
     - Potência: **
 """
+
+#Exemplo
+n1 = 10
+n2 =20
+
+print(n1+n2)
+print(n1-n2)
+print(n1*n2)
+print(n1/n2)
+print(n1//n2)
+print(20%3)
+print(2**3)
+
+# Operadores booleanos
+idade1 = 10
+idade2 = 15
+altura1 = 1.77
+altura2 = 1.65
+altura3 = altura2
+
+print(idade1 > idade2)
+print(idade1 <= idade2)
+print("Python" == "python")
+print("banana" != "abacaxi")
+print(altura1 >= altura2)
+print(altura2 > altura3)
